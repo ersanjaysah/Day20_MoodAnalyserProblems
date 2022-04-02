56627ab029a6e18f30f086da4f61b74e5f9bddf6
sanjay <ssah6694@gmail.com>
2022-04-02 17:17:39 +0530

    [sanjay]added: Message Analyser file

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalizeMood()
+        {
+            if (this.message.Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
