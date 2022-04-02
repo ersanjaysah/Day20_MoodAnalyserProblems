e2827457707042f69589b5a0974587329821fe36
sanjay <ssah6694@gmail.com>
2022-04-02 17:27:32 +0530

    [sanjay]added: Return Sad Mood file

---
+++ MoodAnalyzer/MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in SAD Mood";
+            MoodAnalyser mood = new MoodAnalyser(message);
+
+            // Act
+            string mood1 = mood.AnalizeMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood1);
+        }
+    }
+}
